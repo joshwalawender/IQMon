c1c6e2746e4d82ee560d9839b27194dbadf62af4
Josh Walawender <jmwalawender@gmail.com>
2013-07-16 17:45:20 -1000

    Starting to make OO version of code, not much done yet, just organizing files and experimenting with git.

---
+++ image.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+untitled.py
+
+Created by Josh Walawender on 2013-07-13.
+Copyright (c) 2013 . All rights reserved.
+"""
+
+import sys
+import os
+import unittest
+
+
+class Image(object, filename, config):
+	def __init__(self):
+		pass
+
+
+class untitledTests(unittest.TestCase):
+	def setUp(self):
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()
