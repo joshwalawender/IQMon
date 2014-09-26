3b6acf76cef6bc3ab7ec70a421bce2bd2cd2f642
Josh Walawender <jmwalawender@gmail.com>
2014-09-25 16:35:21 -1000

    Update setup and read me to v1.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "IQMon",
-    version = "1.2",
+    version = "1.3",
     author='Josh Walawender',
     packages = find_packages(),
 )
