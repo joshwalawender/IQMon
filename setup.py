c9e6e7ecb3f434e8e7324453186f3b707a2089cc
Josh Walawender <jmwalawender@gmail.com>
2021-12-03 16:50:49 -1000

    Remove old content

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-from IQMon import __version__
-setup(
-    name = "IQMon",
-    version=__version__,
-    author='Josh Walawender',
-    packages = find_packages(),
-)
