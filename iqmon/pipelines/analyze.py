485e39982f5646a0d979eba31f31b1992296f3cb
Josh Walawender <jmwalawender@gmail.com>
2021-12-16 13:29:12 -1000

    Release memory at end of analysis pipeline

--- iqmon/pipelines/analyze.py
+++ iqmon/pipelines/analyze.py
@@ -1,5 +1,7 @@
 from keckdrpframework.pipelines.base_pipeline import BasePipeline
 from keckdrpframework.models.processing_context import ProcessingContext
+from keckdrpframework.primitives.base_primitive import BasePrimitive
+import numpy as np
 
 # MODIFY THIS IMPORT to reflect the name of the module created in the primitives directory
 from iqmon.primitives.file_handling import (ReadFITS,
@@ -43,8 +45,51 @@ class AnalysisPipeline(BasePipeline):
 #         "get_calibrators": ("GetCalibrationStars", "getting_calibrators", "associate_calibrators"),
 #         "associate_calibrators": ("AssociateCalibratorStars", "associating_calibrators", "render_jpeg"),
         "render_jpeg":       ("RenderJPEG", "rendering_jpeg", "record_analysis"),
-        "record_analysis":   ("RecordFile", "recording", None),
+        "record_analysis":   ("RecordFile", "recording", "release_memory"),
+        "release_memory":    ("ReleaseMemory", "releaseing", None),
     }
 
     def __init__(self, context: ProcessingContext):
         BasePipeline.__init__(self, context)
+
+
+##-----------------------------------------------------------------------------
+## Primitive: ReleaseMemory
+##-----------------------------------------------------------------------------
+class ReleaseMemory(BasePrimitive):
+    """
+    """
+    def __init__(self, action, context):
+        BasePrimitive.__init__(self, action, context)
+        self.log = context.pipeline_logger
+        self.cfg = self.context.config.instrument
+
+    def _pre_condition(self):
+        """Check for conditions necessary to run this process"""
+        checks = []
+        return np.all(checks)
+
+    def _post_condition(self):
+        """
+        Check for conditions necessary to verify that the process ran
+        correctly.
+        """
+        checks = []
+        return np.all(checks)
+
+    def _perform(self):
+        """
+        Returns an Argument() with the parameters that depend on this
+        operation.
+        """
+        self.log.info(f"Running {self.__class__.__name__} action")
+
+        if hasattr(self.action.args, 'ccddata'):
+            del(self.action.args.ccddata)
+        if hasattr(self.action.args, 'background'):
+            del(self.action.args.background)
+        if hasattr(self.action.args, 'objects'):
+            del(self.action.args.objects)
+
+        return self.action.args
+
